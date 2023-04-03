926e9c3e747fd51eae1a4fa9763e00d7d400cbd2
Eryk Olsza <79854472+Kyt00n@users.noreply.github.com>
2023-04-03 18:33:52 +0200

    Override `RankedGradeBook`'s `GetLetterGrade` method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GradeBook.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,51 @@ namespace GradeBook.GradeBooks
     {
         public RankedGradeBook(string name) :base(name) 
         { 
-            Name = name;
             Type = Enums.GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double grade)
+        {
+            
+            
+            
+            if (Students.Count < 5) 
+            {
+                throw new InvalidOperationException();
+            }
+
+            
+            char letterGrade = getTopInClassPercentile(grade) switch
+            {
+                < 20 => 'A',
+                >= 20 and < 40 => 'B',
+                >= 40 and < 60 => 'C',
+                >= 60 and <= 80 => 'D',
+                _ => 'F'
+            };
+            return letterGrade;
+            
+        }
+        private double getTopInClassPercentile(double grade) 
+        {
+            List<double> allGrades = new List<double>();
+            foreach (var student in Students)
+            {
+                allGrades.AddRange(student.Grades);
+            }
+            allGrades.Sort();
+
+            int counter = 0;
+            foreach (double g in allGrades)
+            {
+                if (g > grade)
+                {
+                    break;
+                }
+                counter++;
+            }
+            double topInClass = 100.0 - (counter / (Students.Count * 1.0) * 100.0);
+            return topInClass;
+        }
+        
     }
 }
